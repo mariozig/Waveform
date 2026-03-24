23bb842bb29b43f02c94bafcf4450d2aded5bb98
Mario Zigliotto <mariozig@gmail.com>
2026-03-23 18:02:11 -0700

    fix: bounds-check sampleData access in Highlight.path(in:)
    sampleData is populated asynchronously via GenerateTask on a background
    queue. Highlight.path(in:) could be called during a SwiftUI render pass
    before the callback delivers data, or after a width resize when
    sampleData still has the old count. Both cases cause an Index out of
    range crash.
    
    - Return early when sampleData is empty
    - Clamp startIndex and endIndex to sampleData.count bounds

--- Sources/Waveform/Views/Highlight.swift
+++ Sources/Waveform/Views/Highlight.swift
@@ -2,33 +2,36 @@ import SwiftUI
 
 struct Highlight: Shape {
     let selectedSamples: SampleRange
-    
+
     @EnvironmentObject var generator: WaveformGenerator
-    
+
     func path(in rect: CGRect) -> Path {
         Path { path in
             guard selectedSamples.count > 0, selectedSamples.overlaps(generator.renderSamples) else { return }
-            
+
+            let sampleCount = generator.sampleData.count
+            guard sampleCount > 0 else { return }
+
             let startPosition = max(0, generator.position(of: selectedSamples.lowerBound))
             let endPosition = min(generator.position(of: selectedSamples.upperBound), generator.width)
-            
-            let startIndex = Int(startPosition)
-            let endIndex = Int(endPosition)
-            
+
+            let startIndex = max(0, min(Int(startPosition), sampleCount - 1))
+            let endIndex = max(startIndex, min(Int(endPosition), sampleCount))
+
             path.move(to: CGPoint(x: startPosition, y: rect.midY))
-            
+
             for index in startIndex..<endIndex {
                 let x = CGFloat(index)
                 let max = rect.midY + (rect.midY * CGFloat(generator.sampleData[index].max))
                 path.addLine(to: CGPoint(x: x, y: max))
             }
-            
+
             for index in (startIndex..<endIndex).reversed() {
                 let x = CGFloat(index)
                 let min = rect.midY + (rect.midY * CGFloat(generator.sampleData[index].min))
                 path.addLine(to: CGPoint(x: x, y: min))
             }
-            
+
             path.closeSubpath()
         }
     }
